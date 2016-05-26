831ea80ff577c89b9fd8aab32b71621197525c19
Yoann Cribier <ycribier@student.42.fr>
2016-05-26 13:48:02 +0200

    Saving progress of basic-kigiri-learn 01-each.js - caca prout boudin

--- basic-kigiri-learn/01-each.js
+++ basic-kigiri-learn/01-each.js
@@ -4,10 +4,7 @@
 // fn is the give function
 // collection is the array for the first test
 const each = (fn, collection) => {
-  if (collection)
-  	return _each(fn, collection);
-  else
-    return (collection) => _each.apply(this, [fn, collection]);
+  return collection ? _each(fn, collection) : (collection) => _each.apply(this, [fn, collection]);
 }
 
 const _each = (fn, collection) => {
